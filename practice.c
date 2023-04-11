312120190992aea6448a2595e1b7152777812587
majohnson588 <117084422+majohnson588@users.noreply.github.com>
2023-04-11 11:50:24 +0800

    Update practice.c

--- practice.c
+++ practice.c
@@ -73,3 +73,65 @@ void myStackFree(MyStack* obj) {
 
 	free(obj);
 }
+
+
+//用栈实现队列
+//先复制Stack.c和Stack.h的代码
+typedef struct {
+	Stack pushst;
+	Stack popst;
+}MyQueue;
+
+MyQueue* myQueueCreate() {
+	MyQueue* obj = (MyQueue*)malloc(sizeof(MyQueue));
+	StackInit(&obj->pushst);
+	StackInit(&obj->popst);
+
+	return obj;
+}
+
+void MyQueuePush(MyQueue* obj, int x) {
+	StackPush(&obj->pushst, x);
+}
+
+void MyQueuePop(MyQueue* obj) {
+	if (StackEmpty(&obj->popst))
+	{
+		//如果pop栈为空，则把push栈的数据倒过来
+		while (!StackEmpty(&obj->pushst))
+		{
+			StackPush(&obj->popst, StackTop(&obj->pushst));
+			StackPop(&obj->pushst);
+		}
+	}
+
+	int front = StackTop(&popst);
+	StackPop(&popst);
+	return front;
+}
+
+//取队头
+int myQueuePeek(MyQueue* obj) {
+	if (StackEmpty(&obj->popst))
+	{
+		//如果pop栈为空，则把push栈的数据倒(导)过来
+		while (!StackEmpty(&obj->pushst))
+		{
+			StackPush(&obj->popst, StackTop(&obj->pushst));
+			StackPop(&obj->pushst);
+		}
+	}
+
+	return StackTop(&obj->popst);
+}
+
+bool myQueueEmpty(MyQueue* obj) {
+	return StackEmpty(&obj->popst) && StackEmpty(&obj->pushst);
+}
+
+void myQueueFree(MyQueue* obj) {
+	StackDestroy(&obj->pushst);
+	StackDestroy(&obj->popst);
+
+	free(obj);
+}
